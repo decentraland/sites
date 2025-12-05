c29003f1f95f46e3b3f5c0010133281faaf33f64
Braian Mellor <braianj@gmail.com>
2025-12-05 17:54:50 -0300

    feat: add main

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,28 @@
-import * as React from "react"
-import * as ReactDOM from "react-dom/client"
-import { App } from "./App"
+import * as React from 'react'
+import { IntlProvider } from 'react-intl'
+import { Provider } from 'react-redux'
+import { AnalyticsProvider } from '@dcl/hooks'
+import * as ReactDOM from 'react-dom/client'
+import { DclThemeProvider, darkTheme } from 'decentraland-ui2'
+import { App } from './App'
+import { store } from './app/store'
+import { getEnv } from './config/env'
+import messagesData from './intl/en.json'
+import { flattenMessages } from './intl/flatten'
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+const messages = flattenMessages(messagesData)
+const segmentWriteKey = getEnv('VITE_SEGMENT_WRITE_KEY') || ''
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <DclThemeProvider theme={darkTheme}>
+        <IntlProvider locale="en" messages={messages}>
+          <AnalyticsProvider writeKey={segmentWriteKey}>
+            <App />
+          </AnalyticsProvider>
+        </IntlProvider>
+      </DclThemeProvider>
+    </Provider>
   </React.StrictMode>
 )
