17133add17b72766d992c047d98b52c6f6914c6b
Braian Mellor <braianj@gmail.com>
2026-03-26 18:58:08 -0300

    feat: download page (#75)
    * feat: download page
    
    * feat: refactor download page with shared DownloadOptions and image background
    
    * fix: replace muiIcons with direct @mui/icons-material imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Layout } from './components/Layout'
 import { RemoteLoader } from './components/RemoteLoader'
+import { DownloadPage } from './pages/download'
 import { IndexPage } from './pages/index.tsx'
 import { SignInRedirect } from './pages/SignInRedirect'
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Layout>
-        <Routes>
+      <Routes>
+        <Route path="/download" element={<DownloadPage />} />
+        <Route element={<Layout />}>
           <Route path="/" element={<IndexPage />} />
           <Route path="/sign-in" element={<SignInRedirect />} />
           <Route path="/whats-on/*" element={<RemoteLoader name="whats-on" />} />
-        </Routes>
-      </Layout>
+        </Route>
+      </Routes>
     </BrowserRouter>
   )
 }
