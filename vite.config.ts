24ec8f2ff5939730b51a0bbaceb697d2a08c557d
Braian Mellor <braianj@gmail.com>
2026-03-26 17:18:38 -0300

    fix: make federation remote loading generic and optional (#92)
    * fix: remove broken manifest.json link from index.html
    
    * fix: make federation remote loading generic and optional
    
    * fix: vite

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills'
 // eslint-disable-next-line import/no-default-export
 export default defineConfig(({ command, mode }) => {
   const envVariables = loadEnv(mode, process.cwd())
-
   return {
     resolve: {
       dedupe: ['@emotion/react', '@emotion/styled', '@mui/material']
@@ -18,26 +17,30 @@ export default defineConfig(({ command, mode }) => {
         include: ['buffer']
       }),
       /* eslint-disable @typescript-eslint/naming-convention */
-      federation({
-        name: 'landing_host',
-        remotes: {
-          whats_on: envVariables.VITE_WHATS_ON_REMOTE_URL || 'http://localhost:5001/assets/remoteEntry.js'
-        },
-        shared: {
-          react: { singleton: true, requiredVersion: '^18.3.0' },
-          'react-dom': { singleton: true, requiredVersion: '^18.3.0' },
-          'react-router-dom': { singleton: true, requiredVersion: '^7.9.0' },
-          '@emotion/react': { singleton: true, requiredVersion: '^11.14.0' },
-          '@emotion/styled': { singleton: true, requiredVersion: '^11.14.0' },
-          '@reduxjs/toolkit': { singleton: true, requiredVersion: '^2.9.0' },
-          'react-redux': { singleton: true, requiredVersion: '^9.2.0' },
-          wagmi: { singleton: true, requiredVersion: '^2.19.0' },
-          viem: { singleton: true, requiredVersion: '^2.44.0' },
-          '@dcl/hooks': { singleton: true, requiredVersion: '^1.2.0' },
-          'decentraland-ui2': { singleton: true, requiredVersion: '^1.6.0' },
-          '@dcl/schemas': { singleton: true, requiredVersion: '^25.2.0' }
-        } as Record<string, { singleton: boolean; requiredVersion: string }>
-      })
+      ...(envVariables.VITE_WHATS_ON_REMOTE_URL
+        ? [
+            federation({
+              name: 'landing_host',
+              remotes: {
+                whats_on: envVariables.VITE_WHATS_ON_REMOTE_URL
+              },
+              shared: {
+                react: { singleton: true, requiredVersion: '^18.3.0' },
+                'react-dom': { singleton: true, requiredVersion: '^18.3.0' },
+                'react-router-dom': { singleton: true, requiredVersion: '^7.9.0' },
+                '@emotion/react': { singleton: true, requiredVersion: '^11.14.0' },
+                '@emotion/styled': { singleton: true, requiredVersion: '^11.14.0' },
+                '@reduxjs/toolkit': { singleton: true, requiredVersion: '^2.9.0' },
+                'react-redux': { singleton: true, requiredVersion: '^9.2.0' },
+                wagmi: { singleton: true, requiredVersion: '^2.19.0' },
+                viem: { singleton: true, requiredVersion: '^2.44.0' },
+                '@dcl/hooks': { singleton: true, requiredVersion: '^1.2.0' },
+                'decentraland-ui2': { singleton: true, requiredVersion: '^1.6.0' },
+                '@dcl/schemas': { singleton: true, requiredVersion: '^25.2.0' }
+              } as Record<string, { singleton: boolean; requiredVersion: string }>
+            })
+          ]
+        : [])
       /* eslint-enable @typescript-eslint/naming-convention */
     ],
     build: {
