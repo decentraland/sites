7d049a4ff908a0fa65d96fa8cf3baf43fd72ab82
Braian Mellor <braianj@gmail.com>
2026-03-26 19:15:48 -0300

    feat: download success page (#90)

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Suspense, lazy } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Layout } from './components/Layout'
 import { RemoteLoader } from './components/RemoteLoader'
@@ -5,11 +6,21 @@ import { DownloadPage } from './pages/download'
 import { IndexPage } from './pages/index.tsx'
 import { SignInRedirect } from './pages/SignInRedirect'
 
+const DownloadSuccessPage = lazy(() => import('./pages/DownloadSuccess').then(m => ({ default: m.DownloadSuccess })))
+
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/download" element={<DownloadPage />} />
+        <Route
+          path="/download_success"
+          element={
+            <Suspense fallback={null}>
+              <DownloadSuccessPage />
+            </Suspense>
+          }
+        />
         <Route element={<Layout />}>
           <Route path="/" element={<IndexPage />} />
           <Route path="/sign-in" element={<SignInRedirect />} />
