76a5e86d59ad1789cf1f99660ef2e0f61ba1e5da
Braian Mellor <braianj@gmail.com>
2026-04-01 11:57:05 -0300

    fix: download page mobile layout overflow and avatar preview sizing (#167)
    * fix: download page mobile layout overflow and avatar preview sizing
    
    * feat: add landing footer to download page
    
    * fix: add padding to footer wrapper to prevent share bar overlap on mobile

--- src/components/Layout/DownloadLayout.tsx
+++ src/components/Layout/DownloadLayout.tsx
@@ -12,6 +12,7 @@ import { getEnv } from '../../config/env'
 import { useGetProfileQuery } from '../../features/profile/profile.client'
 import { useFormatMessage } from '../../hooks/adapters/useFormatMessage'
 import { DownloadOptions } from '../DownloadOptions'
+import { LandingFooter } from '../LandingFooter'
 import { WrapDecentralandText } from '../WrapDecentralandText'
 import { DownloadLayoutProps } from './DownloadLayout.types'
 import {
@@ -26,6 +27,7 @@ import {
   DownloadTitle,
   DownloadWearablePreviewContainer,
   DownloadWearablePreviewOverlay,
+  FooterWrapper,
   MobileTitle,
   Modal,
   ModalContent,
@@ -127,88 +129,93 @@ const DownloadLayout = memo((props: DownloadLayoutProps) => {
   }, [l])
 
   return (
-    <DownloadPageContainer>
-      <DownloadContainer>
-        <DclLogo onClick={() => (window.location.href = 'https://decentraland.org')} />
+    <>
+      <DownloadPageContainer>
+        <DownloadContainer>
+          <DclLogo onClick={() => (window.location.href = 'https://decentraland.org')} />
 
-        {isDesktop && (
-          <>
-            <AlreadyDownloadedContainer>
-              <AlreadyDownloadedText>
-                {l('page.download.already_downloaded')}{' '}
-                <AlreadyDownloadedLink onClick={handleJumpIn}>{l('page.download.jump_in')}</AlreadyDownloadedLink>
-              </AlreadyDownloadedText>
-            </AlreadyDownloadedContainer>
-            <DownloadOptionsContainer>
-              <PreTitleContainer>
-                <CheckCircleIcon htmlColor="#34CE77" fontSize="large" />
-                <Typography variant="h4">
-                  {l('page.download.pre_title', {
-                    name: profileName || l('page.download.your_account')
-                  })}
-                </Typography>
-              </PreTitleContainer>
-              <DownloadTitle variant="h2">
-                <WrapDecentralandText text={title} />
-              </DownloadTitle>
-              <DownloadOptions />
-            </DownloadOptionsContainer>
-          </>
+          {isDesktop && (
+            <>
+              <AlreadyDownloadedContainer>
+                <AlreadyDownloadedText>
+                  {l('page.download.already_downloaded')}{' '}
+                  <AlreadyDownloadedLink onClick={handleJumpIn}>{l('page.download.jump_in')}</AlreadyDownloadedLink>
+                </AlreadyDownloadedText>
+              </AlreadyDownloadedContainer>
+              <DownloadOptionsContainer>
+                <PreTitleContainer>
+                  <CheckCircleIcon htmlColor="#34CE77" fontSize="large" />
+                  <Typography variant="h4">
+                    {l('page.download.pre_title', {
+                      name: profileName || l('page.download.your_account')
+                    })}
+                  </Typography>
+                </PreTitleContainer>
+                <DownloadTitle variant="h2">
+                  <WrapDecentralandText text={title} />
+                </DownloadTitle>
+                <DownloadOptions />
+              </DownloadOptionsContainer>
+            </>
+          )}
+
+          {!isDesktop && (
+            <MobileTitle variant="h2">
+              <WrapDecentralandText text={title} />
+            </MobileTitle>
+          )}
+
+          <DownloadImageContainer>
+            {!isDesktop && <DownloadWearablePreviewOverlay />}
+            <DownloadWearablePreviewContainer
+              ref={(node: HTMLDivElement | null) => {
+                wearableRef(node)
+                wearableContainerRef.current = node
+              }}
+            >
+              {WearablePreviewComponent && (
+                <WearablePreviewComponent
+                  unity
+                  unityMode="jesus"
+                  profile={profile?.avatars?.[0]?.ethAddress || randomDefaultProfile}
+                  disableBackground={true}
+                  lockBeta={true}
+                  dev={false}
+                  baseUrl={getEnv('WEARABLE_PREVIEW_URL')}
+                />
+              )}
+            </DownloadWearablePreviewContainer>
+          </DownloadImageContainer>
+        </DownloadContainer>
+
+        {!isDesktop && typeof navigator !== 'undefined' && !!navigator.share && (
+          <ShareContainer>
+            <Typography variant="h6">{l('page.download.mobile.switch_to_computer')}</Typography>
+            <ShareButton onClick={handleShare} endIcon={<ShareOutlinedIcon />} variant="contained">
+              {l('page.download.mobile.send_link')}
+            </ShareButton>
+          </ShareContainer>
         )}
-
-        {!isDesktop && (
-          <MobileTitle variant="h2">
-            <WrapDecentralandText text={title} />
-          </MobileTitle>
-        )}
-
-        <DownloadImageContainer>
-          {!isDesktop && <DownloadWearablePreviewOverlay />}
-          <DownloadWearablePreviewContainer
-            ref={(node: HTMLDivElement | null) => {
-              wearableRef(node)
-              wearableContainerRef.current = node
-            }}
-          >
-            {WearablePreviewComponent && (
-              <WearablePreviewComponent
-                unity
-                unityMode="jesus"
-                profile={profile?.avatars?.[0]?.ethAddress || randomDefaultProfile}
-                disableBackground={true}
-                lockBeta={true}
-                dev={false}
-                baseUrl={getEnv('WEARABLE_PREVIEW_URL')}
-              />
-            )}
-          </DownloadWearablePreviewContainer>
-        </DownloadImageContainer>
-      </DownloadContainer>
-
-      {!isDesktop && typeof navigator !== 'undefined' && !!navigator.share && (
-        <ShareContainer>
-          <Typography variant="h6">{l('page.download.mobile.switch_to_computer')}</Typography>
-          <ShareButton onClick={handleShare} endIcon={<ShareOutlinedIcon />} variant="contained">
-            {l('page.download.mobile.send_link')}
-          </ShareButton>
-        </ShareContainer>
-      )}
-      <Modal open={openModal} size="tiny">
-        <ModalContent>
-          <ModalIcon>
-            <FileDownloadOutlinedIcon />
-          </ModalIcon>
-          <ModalTitle>
-            {l('page.download.modal.title_first_line')}
-            <br />
-            {l('page.download.modal.title_second_line')}
-          </ModalTitle>
-          <Button variant="contained" onClick={() => setOpenModal(false)}>
-            {l('page.download.modal.cta')}
-          </Button>
-        </ModalContent>
-      </Modal>
-    </DownloadPageContainer>
+        <Modal open={openModal} size="tiny">
+          <ModalContent>
+            <ModalIcon>
+              <FileDownloadOutlinedIcon />
+            </ModalIcon>
+            <ModalTitle>
+              {l('page.download.modal.title_first_line')}
+              <br />
+              {l('page.download.modal.title_second_line')}
+            </ModalTitle>
+            <Button variant="contained" onClick={() => setOpenModal(false)}>
+              {l('page.download.modal.cta')}
+            </Button>
+          </ModalContent>
+        </Modal>
+      </DownloadPageContainer>
+      <FooterWrapper>
+        <LandingFooter />
+      </FooterWrapper>
+    </>
   )
 })
 
