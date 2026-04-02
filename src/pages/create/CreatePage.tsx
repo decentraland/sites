c86a6d956d66478e4e1be05429437a26656b96ca
Braian Mellor <braianj@gmail.com>
2026-04-02 16:21:22 -0300

    feat: lazy load create page sections for better performance

--- src/pages/create/CreatePage.tsx
+++ src/pages/create/CreatePage.tsx
@@ -1,22 +1,25 @@
-import { memo } from 'react'
-import { CreatorsConnect } from '../../components/Create/Connect'
-import { CreatorsCreate } from '../../components/Create/CreateCards'
-import { CreatorsEarn } from '../../components/Create/Earn'
-import { CreatorsFaqs } from '../../components/Create/Faqs'
+import { Suspense, lazy, memo } from 'react'
 import { CreatorsHero } from '../../components/Create/Hero'
-import { CreatorsLearn } from '../../components/Create/Learn'
-import { CreatorsWhy } from '../../components/Create/Why'
+
+const CreatorsWhy = lazy(() => import('../../components/Create/Why').then(m => ({ default: m.CreatorsWhy })))
+const CreatorsCreate = lazy(() => import('../../components/Create/CreateCards').then(m => ({ default: m.CreatorsCreate })))
+const CreatorsConnect = lazy(() => import('../../components/Create/Connect').then(m => ({ default: m.CreatorsConnect })))
+const CreatorsLearn = lazy(() => import('../../components/Create/Learn').then(m => ({ default: m.CreatorsLearn })))
+const CreatorsEarn = lazy(() => import('../../components/Create/Earn').then(m => ({ default: m.CreatorsEarn })))
+const CreatorsFaqs = lazy(() => import('../../components/Create/Faqs').then(m => ({ default: m.CreatorsFaqs })))
 
 const CreatePage = memo(() => {
   return (
     <>
       <CreatorsHero />
-      <CreatorsWhy />
-      <CreatorsCreate />
-      <CreatorsConnect />
-      <CreatorsLearn />
-      <CreatorsEarn />
-      <CreatorsFaqs />
+      <Suspense fallback={null}>
+        <CreatorsWhy />
+        <CreatorsCreate />
+        <CreatorsConnect />
+        <CreatorsLearn />
+        <CreatorsEarn />
+        <CreatorsFaqs />
+      </Suspense>
     </>
   )
 })
