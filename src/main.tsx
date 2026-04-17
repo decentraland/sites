c57820984f85ed1e0476f861fb42cb1308120353
Juanma Hidalgo <juanma06@gmail.com>
2026-04-17 13:34:08 +0200

    feat: remove Redux and initialize Sentry (#271)

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Provider } from 'react-redux'
 import { AnalyticsProvider } from '@dcl/hooks'
 import { DclThemeProvider, darkTheme } from 'decentraland-ui2'
 import { App } from './App'
-import { store } from './app/store'
 import { getEnv } from './config/env'
 import { LocaleProvider } from './intl/LocaleContext'
+import './modules/sentry'
 
 const segmentWriteKey = getEnv('SEGMENT_KEY') || ''
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Provider store={store}>
-      <DclThemeProvider theme={darkTheme}>
-        <LocaleProvider>
-          <AnalyticsProvider writeKey={segmentWriteKey}>
-            <App />
-          </AnalyticsProvider>
-        </LocaleProvider>
-      </DclThemeProvider>
-    </Provider>
+    <DclThemeProvider theme={darkTheme}>
+      <LocaleProvider>
+        <AnalyticsProvider writeKey={segmentWriteKey}>
+          <App />
+        </AnalyticsProvider>
+      </LocaleProvider>
+    </DclThemeProvider>
   </StrictMode>
 )
