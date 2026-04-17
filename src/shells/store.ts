9fbb3188edf6a2aa7967892a442d254c403c0c07
Braian Mellor <braianj@gmail.com>
2026-04-17 15:35:14 -0300

    feat: wire blog reducers (persisted) and RTK Query clients into store

--- src/shells/store.ts
+++ src/shells/store.ts
@@ -1,15 +1,43 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE, persistReducer, persistStore } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import type { TypedUseSelectorHook } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { blogReducer } from '../features/blog/blog.slice'
 import { eventsClient } from '../features/explore-events/events.client'
+import { algoliaClient, cmsClient } from '../services/blogClient'
 
-// PR3 will add blog, cmsClient, algoliaClient reducers and redux-persist.
-// Typed hooks (useAppDispatch/useAppSelector) are not exported yet — explore
-// code consumes RTK Query auto-generated hooks which dispatch internally.
+const blogPersistConfig = {
+  key: 'blog',
+  storage,
+  whitelist: ['ids', 'entities']
+}
+
+const rootReducer = combineReducers({
+  [eventsClient.reducerPath]: eventsClient.reducer,
+  blog: persistReducer(blogPersistConfig, blogReducer),
+  [cmsClient.reducerPath]: cmsClient.reducer,
+  [algoliaClient.reducerPath]: algoliaClient.reducer
+})
+
 const store = configureStore({
-  reducer: {
-    [eventsClient.reducerPath]: eventsClient.reducer
-  },
-  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(eventsClient.middleware),
+  reducer: rootReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+      }
+    }).concat(eventsClient.middleware, cmsClient.middleware, algoliaClient.middleware),
   devTools: import.meta.env.DEV
 })
 
-export { store }
+const persistor = persistStore(store)
+
+type RootState = ReturnType<typeof store.getState>
+type AppDispatch = typeof store.dispatch
+
+const useAppDispatch = () => useDispatch<AppDispatch>()
+const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+
+export { store, persistor, useAppDispatch, useAppSelector }
+export type { RootState, AppDispatch }
