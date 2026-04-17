73c0a0c98ad2b0b40c236949d20acf94f9da8730
Braian Mellor <braianj@gmail.com>
2026-04-17 16:37:56 -0300

    fix: add 96px top clearance to BlogLayout so content clears fixed navbar
    Landing's navbar is position:fixed, top:0 with height 64px mobile / 92px
    desktop. Blog content was rendering at top:0 and hiding behind it. Added
    paddingTop 64px (mobile) and 96px (desktop, 92px navbar + 4px buffer) to
    BlogLayoutContainer.

--- src/components/blog/BlogLayout.styled.ts
+++ src/components/blog/BlogLayout.styled.ts
@@ -1,10 +1,14 @@
 import { Box, styled } from 'decentraland-ui2'
 
-const BlogLayoutContainer = styled(Box)({
+const BlogLayoutContainer = styled(Box)(({ theme }) => ({
   display: 'flex',
   flexDirection: 'column',
-  width: '100%'
-})
+  width: '100%',
+  paddingTop: 64, // mobile navbar height (px)
+  [theme.breakpoints.up('md')]: {
+    paddingTop: 96 // desktop navbar height 92px + 4px buffer
+  }
+}))
 
 const BlogContentWrapper = styled(Box)(({ theme }) => ({
   flex: 1,
