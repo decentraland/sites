8a351c54b6c521da90e6c852ca1b53a489e270ed
Braian Mellor <braianj@gmail.com>
2026-04-21 22:11:29 -0300

    feat: register admin client in DappsShell store

--- src/shells/store.ts
+++ src/shells/store.ts
@@ -2,11 +2,13 @@ import { useDispatch, useSelector } from 'react-redux'
 import type { TypedUseSelectorHook } from 'react-redux'
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import { blogReducer } from '../features/blog/blog.slice'
+import { adminClient } from '../features/whats-on/admin'
 import { eventsClient } from '../features/whats-on-events/events.client'
 import { algoliaClient, cmsClient } from '../services/blogClient'
 
 const rootReducer = combineReducers({
   [eventsClient.reducerPath]: eventsClient.reducer,
+  [adminClient.reducerPath]: adminClient.reducer,
   blog: blogReducer,
   [cmsClient.reducerPath]: cmsClient.reducer,
   [algoliaClient.reducerPath]: algoliaClient.reducer
@@ -15,7 +17,7 @@ const rootReducer = combineReducers({
 const store = configureStore({
   reducer: rootReducer,
   middleware: getDefaultMiddleware =>
-    getDefaultMiddleware().concat(eventsClient.middleware, cmsClient.middleware, algoliaClient.middleware),
+    getDefaultMiddleware().concat(eventsClient.middleware, adminClient.middleware, cmsClient.middleware, algoliaClient.middleware),
   devTools: import.meta.env.DEV
 })
 
