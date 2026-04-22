48daa2e7cf4713ba7646bef3b3b647d152251c9a
Juanma Hidalgo <juanma06@gmail.com>
2026-04-22 17:44:38 +0200

    feat: update mobile links

--- src/components/Home/shared/googlePlay.ts
+++ src/components/Home/shared/googlePlay.ts
@@ -1,10 +1,10 @@
 import { assetUrl } from '../../../utils/assetUrl'
 
 const GOOGLE_PLAY_MOBILE_URL =
-  'https://play.google.com/store/apps/details?id=org.decentraland.godotexplorer&pcampaignid=web_share&utm_source=dcl_foundation&utm_medium=internal&utm_campaign=mobile_launch&utm_content=android&pli=1'
+  'https://play.google.com/store/apps/details?id=org.decentraland.godotexplorer&utm_org=dclrgl&utm_source=fdn&utm_medium=qr&utm_campaign=dclpage&utm_content=android'
 
 const GOOGLE_PLAY_DESKTOP_URL =
-  'https://play.google.com/store/apps/details?id=org.decentraland.godotexplorer&pcampaignid=web_share&utm_source=partners&utm_medium=pr&utm_campaign=mobile_launch&utm_content=android'
+  'https://play.google.com/store/apps/details?id=org.decentraland.godotexplorer&utm_org=dclrgl&utm_source=fdn&utm_medium=qr&utm_campaign=dclpage&utm_content=android'
 
 const googlePlayBadge = assetUrl('/google_play_cta.svg')
 
