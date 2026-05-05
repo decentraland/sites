1abe8b32ca2512ef9c9d3b374e0d24d9a1e1e520
Juanma Hidalgo <juanma06@gmail.com>
2026-05-05 11:03:58 +0200

    Revert "fix: keep download backdrop visible until the gateway finishes servin…" (#388)
    This reverts commit 9b9a38f7d5cfaf0fc95c54cbe48af3dd635253d7.

--- src/modules/file.types.ts
+++
@@ -1,4 +0,0 @@
-type DownloadProgress = { loaded: number; total: number }
-type DownloadProgressCallback = (progress: DownloadProgress) => void
-
-export type { DownloadProgress, DownloadProgressCallback }
--- src/modules/streamOrFallback.types.ts
+++
@@ -1,8 +0,0 @@
-type StreamOrFallbackArgs = {
-  url: string
-  filename: string
-  signal: AbortSignal
-  onProgress: (progress: number) => void
-}
-
-export type { StreamOrFallbackArgs }
